0a35b59471bf670674694ffc61cee05c73e28d21
Grant Robert Thompson <58478508+Tower5954@users.noreply.github.com>
2021-02-07 05:48:14 +0000

    Add files via upload

---
+++ puppa.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import cv2
+
+img = cv2.imread('DATA/00-puppy.jpg')
+
+cv2.imshow('puppa', img)
+
+cv2.waitKey()
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+print("puppa")
+
+
+# In[ ]:
+
+
+
+
